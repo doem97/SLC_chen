be8359ab99c5baedd920ac87b867377e995405dc
doem97 <doem1997@gmail.com>
2019-05-20 17:10:55 +0800

    add resnet densenet

--- code/models/__init__.py
+++ code/models/__init__.py
@@ -1,1 +1,2 @@
-from .resnet import resnet_v1, resnet_v2
+from .resnet import resnet_v1, resnet_v2, ResNet50, ResNet101, ResNet152, ResNet50V2, ResNet101V2, ResNet152V2, ResNeXt50, ResNeXt101
+from .densenet import DenseNet121, DenseNet169, DenseNet201
---
+++ code/models/densenet.py
@@ -0,0 +1,21 @@
+import keras
+from keras.models import Model
+from keras.layers import Dense
+
+def DenseNet121(input_shape, output_shape, **kwargs):
+    model = keras.applications.densenet.DenseNet121(include_top=False, weights='imagenet', input_tensor=None, input_shape=input_shape, pooling='max')
+    output_layer = Dense(output_shape, activation='softmax', kernel_initializer='he_normal')(model.output)
+    model = Model(inputs= model.input, outputs= output_layer)
+    return model
+
+def DenseNet169(input_shape, output_shape, **kwargs):
+    model = keras.applications.densenet.DenseNet169(include_top=False, weights='imagenet', input_tensor=None, input_shape=input_shape, pooling='max')
+    output_layer = Dense(output_shape, activation='softmax', kernel_initializer='he_normal')(model.output)
+    model = Model(inputs= model.input, outputs= output_layer)
+    return model
+
+def DenseNet201(input_shape, output_shape, **kwargs):
+    model = keras.applications.densenet.DenseNet201(include_top=False, weights='imagenet', input_tensor=None, input_shape=input_shape, pooling='max')
+    output_layer = Dense(output_shape, activation='softmax', kernel_initializer='he_normal')(model.output)
+    model = Model(inputs= model.input, outputs= output_layer)
+    return model
